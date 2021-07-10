a1cce8b1b9f6a6b5dddc04e7f77f2524e7e6a70d
ksoer <ksoerr@gmail.com>
2021-07-10 11:26:12 +0300

    Робоча версія

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-// import './sass/main.scss';
-import debounce from 'lodash.debounce';
-import api from './js/fetchCountries';
-import { error } from '@pnotify/core';
+import './js/country';
+import './style.css';
--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-function fetchCountries(searchQuery) {
-    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
-    return fetch(url)
-      .then(r => r.json())
-      .catch(error => console.log(error));
-  }
-  
-  export default fetchCountries;
